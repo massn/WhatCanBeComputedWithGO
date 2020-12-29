9dcac36547a54134aaddf4efe5f9276a8b9e9379
Massn <massn23579@gmail.com>
2020-12-29 21:28:55 +0900

    :hammer:[chap3]add count_lines test

--- chap3/count_lines_test.go
+++ chap3/count_lines_test.go
@@ -1,11 +1,31 @@
 package chap3
 
 import (
-	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCountLines(t *testing.T) {
+type CountLinesTestSuite struct {
+	suite.Suite
+}
+
+func TestCountLinesTestSuite(t *testing.T) {
+	suite.Run(t, new(CountLinesTestSuite))
+}
+
+func (suite *CountLinesTestSuite) TestNormal() {
 	length := countLines("hoge\nfuga\npoyo")
-	assert.Equal(t, "3", length)
+	assert.Equal(suite.T(), "3", length)
+}
+
+func (suite *CountLinesTestSuite) TestSelfReflection() {
+	bytes, err := ioutil.ReadFile("count_lines.go")
+	if err != nil {
+		panic(err)
+	}
+	length := countLines(string(bytes))
+	assert.Equal(suite.T(), "12", length)
 }
